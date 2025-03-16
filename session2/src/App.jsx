2443ab0f30aa5732949e32ab66efeed591b97867
govind-ik <jeeaspirant4567@gmail.com>
2025-03-16 11:23:55 +0000

    learing api with axios

--- session2/src/App.jsx
+++ session2/src/App.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import Cards from './components/Cards.jsx';
+import axios from 'axios';
 
 
 
@@ -16,33 +17,48 @@ const App = () => {
   // const clas="14";
   // const city="prayagraj"
 
-  const user1=[
-    {
-      "name": "John Doe",
-      "profession": "Software Engineer",
-      "city": "New York, USA"
-    },
-    {
-      "name": "Alice Smith",
-      "profession": "Graphic Designer",
-      "city": "Los Angeles, USA"
-    },
-    {
-      "name": "Rahul Sharma",
-      "profession": "Data Scientist",
-      "city": "Bangalore, India"
-    },
-    {
-      "name": "Sophia Martinez",
-      "profession": "Marketing Manager",
-      "city": "Toronto, Canada"
-    },
-    {
-      "name": "Liam O'Connor",
-      "profession": "Cybersecurity Analyst",
-      "city": "Dublin, Ireland"
-    }
-  ]
+  // const user1=[
+  //   {
+  //     "name": "John Doe",
+  //     "profession": "Software Engineer",
+  //     "city": "New York, USA"
+  //   },
+  //   {
+  //     "name": "Alice Smith",
+  //     "profession": "Graphic Designer",
+  //     "city": "Los Angeles, USA"
+  //   },
+  //   {
+  //     "name": "Rahul Sharma",
+  //     "profession": "Data Scientist",
+  //     "city": "Bangalore, India"
+  //   },
+  //   {
+  //     "name": "Sophia Martinez",
+  //     "profession": "Marketing Manager",
+  //     "city": "Toronto, Canada"
+  //   },
+  //   {
+  //     "name": "Liam O'Connor",
+  //     "profession": "Cybersecurity Analyst",
+  //     "city": "Dublin, Ireland"
+  //   }
+  // ]
+  const [d, setData] = useState([])
+  const f=async()=>{
+    const resp=await axios.get('https://picsum.photos/v2/list?page=2&limit=10');
+    setData(resp.data);
+    console.log(d);
+   
+
+  }
+  // useEffect(() => {
+  //   console.log(data); // ✅ This will log the updated state after it changes
+  // }, [data]);
+  
+ //error is we are never calling function jo ki krega to agar f ko hata de to chal jayega wrna ek button bna lete h on click pe chle.
+
+ 
   
 
   return (
@@ -61,11 +77,19 @@ const App = () => {
       <button>press</button>
       </form>
       */}
-     <div>
+     {/* <div>
       {user1.map(function(e){
         return <Cards name={e.name} profession={e.profession} city={e.city} />
       })}
-     </div>
+     </div> */}
+     <button onClick={f}>get data</button>
+     {d.map(function(e,idx){
+      return <div key={idx}>
+        <Cards photo={e.
+download_url} />
+
+        </div>
+     })}
       
      
 
--- session2/src/components/Cards.jsx
+++ session2/src/components/Cards.jsx
@@ -3,11 +3,10 @@ import React from 'react'
 const Cards = (props) => {
     console.log(props)
   return (
-    <div class="card">
-  <h2 class="name">{props.name}</h2>
-  <p class="profession">{props.profession}</p>
-  <p class="city">{props.city}</p>
-</div>
+    <div className='card'>
+      <img src={props.photo}/>
+      
+    </div>
 
   )
 }
